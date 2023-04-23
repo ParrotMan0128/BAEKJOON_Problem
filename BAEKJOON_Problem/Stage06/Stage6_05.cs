e2103c13bb48f4a6607d9e27f11aa2e1dc239bdc
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-04-23 21:23:17 +0900

    Stage6 코딩중...

---
+++ BAEKJOON_Problem/Stage06/Stage6_05.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON_Problem.Stage06
+{
+    internal class Stage6_05
+    {
+
+    
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            char[] charInput = input.ToArray();
+            Array.Reverse(charInput);
+
+            string Reversed = new string(charInput);
+
+            if (input == Reversed)
+            {
+
+                Console.WriteLine(1);
+
+            } else
+            {
+
+                Console.WriteLine(0);
+
+            }
+
+        }
+        
+    }
+}
