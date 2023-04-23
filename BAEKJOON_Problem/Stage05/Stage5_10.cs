7259d33f9583e2cfce9850f7e36eae754a2cfc90
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-04-23 20:48:23 +0900

    Stage5 추가

---
+++ BAEKJOON_Problem/Stage05/Stage5_10.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON_Problem.Stage05
+{
+    internal class Stage5_10
+    {
+
+        static void Main(string[] args)
+        {
+
+            string[] alphabets = { "", "", "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+            char[] input = Console.ReadLine().ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                for (int j = 3; j < alphabets.Length; j++)
+                {
+
+                    if (alphabets[j].Contains(input[i]))
+                    {
+
+                        sum += j;
+
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ BAEKJOON_Problem/Stage05/Stage5_11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON_Problem.Stage05
+{
+    internal class Stage5_11
+    {
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                string input = Console.ReadLine();
+
+                if (input == null) { break; }
+
+                Console.WriteLine(input);
+
+            }
+
+        }
+
+    }
+}
