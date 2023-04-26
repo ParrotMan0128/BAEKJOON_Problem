9e7dcc2021e1ccdb7c8b4110f0c73a9c798d4f53
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-04-27 02:45:59 +0900

    Stage8 코딩중

---
+++ BAEKJOON_Problem/Stage08/Stage8_01.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace BAEKJOON_Problem.Stage08
+{
+    internal class Stage8_01
+    {
+
+        static void Main(string[] args)
+        {
+
+            var inputs = Console.ReadLine().Split(' ');
+
+            var N = inputs[0];
+            var B = int.Parse(inputs[1]);
+
+            var Length = N.Length - 1;
+
+            var result = 0;
+
+            for (var i = 0; i <= Length; i++)
+            {
+
+                var Num = (int)Math.Pow(B, Length - i);
+                result += N[i] >= '0' && N[i] <= '9' ? (N[i] - '0') * Num : (N[i] - 55) * Num;
+
+            }
+
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
