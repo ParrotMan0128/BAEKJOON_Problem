391b86c7a3dc65a8c9bce3c9fcfaa051576cb9e4
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-05-16 19:03:41 +0900

    Stage 13 5번 추가

---
+++ BAEKJOON_Problem/Stage13/Stage13_05.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON_Problem.Stage13
+{
+    internal class Stage13_05
+    {
+
+        static void Main(string[] args)
+        {
+
+            const int MAX_NUM = 10001;
+
+            int n = int.Parse(Console.ReadLine());
+            int[] freq = new int[MAX_NUM];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                freq[int.Parse(Console.ReadLine())]++;
+
+            }
+
+            using (var print = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+
+                for (int i = 0; i < MAX_NUM; i++)
+                {
+
+                    if (freq[i] == 0) { continue; }
+
+                    for (int j = 0; j < freq[i]; j++)
+                    {
+
+                        print.WriteLine(i);
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
