12157ec2c914e67070c0a254998f548785603533
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-05-17 14:53:57 +0900

    Stage 8 3번 추가

---
+++ BAEKJOON_Problem/Stage08/Stage8_03.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON_Problem.Stage08 {
+    internal class Stage8_03 {
+
+        static void Main(string[] args) {
+
+            int testCase = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testCase; i++) {
+
+                int change = int.Parse(Console.ReadLine());
+                int quarter = change / 25;
+                change %= 25;
+                int dime = change / 10;
+                change %= 10;
+                int nickel = change / 5;
+                change %= 5;
+                int penny = change;
+
+                Console.WriteLine("{0} {1} {2} {3}", quarter, dime, nickel, penny);
+            }
+
+        }
+
+
+    }
+}
