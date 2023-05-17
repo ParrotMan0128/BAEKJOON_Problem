7307a71357f61bf677dc452eb018018dca363fbd
ParrotMan_ <92490137+ParrotMan0128@users.noreply.github.com>
2023-05-17 22:59:49 +0900

    Stage 14 1 추가

---
+++ BAEKJOON_Problem/Stage14/Stage14_01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON_Problem.Stage14
+{
+    internal class Stage14_01
+    {
+
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+        }
+
+    }
+}
